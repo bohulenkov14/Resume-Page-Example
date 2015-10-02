42c26f37a5c8e5f75a9b0058c243d3fd070a31fa
bohulenkov.a <bohulenkov14@gmail.com>
2015-10-02 18:54:20 +0300

    feat: Added some js objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,44 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var work = {
+  "jobs": [
+  {
+    "employer": "MirKvartir",
+    "title": "Junior Web Developer",
+    "location": "Moscow",
+    "dates": "03.08.2015-now",
+    "description": "Development of real estate ads website"
+  },
+  {
+    "employer": "Logos Agent Technologies LLC",
+    "title": "C++ Developer",
+    "location": "Moscow",
+    "dates": "03.02.2014-23.06.2015",
+    "description": "Development of training simulators"
+  },
+  {
+    "employer": "Mindbox LLC",
+    "title": "Junior Web Developer",
+    "location": "Moscow",
+    "dates": "03.08.2015-now",
+    "description": "Development of CRM systems"
+  },
+  {
+    "employer": "Mail.ru",
+    "title": "Junior Android Developer",
+    "location": "Moscow",
+    "dates": "05.11.2011-05.06.2012",
+    "description": "Development of instant messagers"
+  }]
+}
+
+var projects = {
+  "projects": [
+  {
+    "title":,
+    "dates":,
+    "description":
+    "images":[]
+  },
+
+  ]
+}
+
