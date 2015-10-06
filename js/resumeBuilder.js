d8363f317ca9c1fdef22e541f2ab9ade9e091441
Bohulenkov Artem <bohulenkov14@gmail.com>
2015-10-06 08:35:39 +0300

    feat:Added JSON objects with resume data

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -33,12 +33,48 @@ var work = {
 var projects = {
   "projects": [
   {
-    "title":,
-    "dates":,
-    "description":
+    "title": "Distributed Computing Framework",
+    "dates": "01.09.2012 - 30.06.2013",
+    "description": "Framework for building distributed computing systems"
     "images":[]
   },
-
-  ]
+  {
+    "title": "PCA clustering algorithms research",
+    "dates": "01.09.2012 - 31.12.2012",
+    "description": "research work on prinipal component analysis for text clustering"
+    "images":[]
+  }]
 }
 
+var bio = {
+  "name": "Artem Bohulenkov",
+  "role": "Web Developer",
+  "welcomeMessage": "Hello !",
+  "contacts": {
+    "mobile": "+7 (915)398-09-45",
+    "email": "bohulenkov14@gmail.com",
+    "github": "bohulenkov14",
+    "twitter": "@zellos_dajedra",
+    "location": "Moscow"
+  },
+  "skills": ["C#", "C++", "HTML", "CSS", "ASP.NET MVC", "git", "hg", "svn"]
+}
+
+var education = {
+  "schools": [
+  {
+    "name": "National Research Nuclear University",
+    "location": "Moscow",
+    "degree": "specialist (equivalent to Masters)",
+    "majors": ["CS", "Mathematics"],
+    "dates": "01.01.2009 - 01.06.2014",
+    "url": "http://www.mephi.ru"
+  }],
+  onlineCourses: [
+  {
+    "title": "Udacity Front-End developer nanodegree"
+    "schoold": "Udacity",
+    "dates": "13.09.2015 - now",
+    "url":"http://www.udacity.com"
+  }]
+}
