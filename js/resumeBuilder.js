c2e30b9db849eac0b91de7042f810bd6c7e20f72
bohulenkov.a <bohulenkov14@gmail.com>
2015-10-06 18:22:38 +0300

    refactor: moved js code from index.html to resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -30,15 +30,65 @@ var work = {
   }]
 }
 
-var projects = {
-  "projects": [
-  {
-    "title":,
-    "dates":,
-    "description":
-    "images":[]
-  },
-
-  ]
+var bio = {
+  "skills": ["skillOne", "skillTwo"]
 }
 
+
+
+function displayWork()
+{
+  if(document.getElementsByClassName('flex-item').length === 0) {
+      document.getElementById('topContacts').style.display = 'none';
+    }
+    if (bio.skills.length > 0)
+    {
+      $("#header").append(HTMLskillsStart);
+      for(var i = 0; i < bio.skills.length; ++i)
+      {
+        var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+        $("#skills").append(formattedSkill);
+      }
+    }
+
+    if (work.jobs.length > 0)
+    {
+
+      for(workEntry in work.jobs)
+      {
+        $("#workExperience").append(HTMLworkStart)
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[workEntry].employer);
+        var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[workEntry].title);
+        $(".work-entry:last").append(formattedEmployer + formattedWorkTitle);
+
+        var formattedDate = HTMLworkDates.replace("%data%", work.jobs[workEntry].dates)
+        $(".work-entry:last").append(formattedDate);
+
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[workEntry].description)
+        $(".work-entry:last").append(formattedDescription);
+
+      }
+    }
+
+    if(document.getElementsByClassName('work-entry').length === 0) {
+      document.getElementById('workExperience').style.display = 'none';
+    }
+    if(document.getElementsByClassName('project-entry').length === 0) {
+      document.getElementById('projects').style.display = 'none';
+    }
+    if(document.getElementsByClassName('education-entry').length === 0) {
+      document.getElementById('education').style.display = 'none';
+    }
+    if(document.getElementsByClassName('flex-item').length === 0) {
+      document.getElementById('lets-connect').style.display = 'none';
+    }
+    if(document.getElementById('map') === null) {
+      document.getElementById('mapDiv').style.display = 'none';
+    }
+}
+
+displayWork();
+
+$(document).click(function(loc){
+  logClicks(loc.pageX, loc.pageY);
+})
